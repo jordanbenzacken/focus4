0077f4254c7b38e75d50cce56b8b4018abd9155c
Damien Frikha <damien.frikha@outlook.com>
2017-01-08 21:06:06 +0100

    Ajout status "ignored" dans le RequestStore

--- src/network/fetch.ts
+++ src/network/fetch.ts
@@ -72,9 +72,5 @@ export async function httpPut<RQ, RS>(url: string, data: RQ): Promise<RS> {
 export function cancellableFetch<RQ, RS>(method: "GET" | "POST" | "PUT" | "DELETE", url: string, data?: RQ, success?: (data: RS) => void, error?: (e: any) => void) {
     const id = v4();
     coreFetch<RQ, RS>(method, url, data, id).then(success, error);
-    return () => {
-        if (requestStore.pending.has(id)) {
-            requestStore.pending.delete(id);
-        }
-    };
+    return () => requestStore.updateRequest({id, url, status: "ignored"});
 }
--- src/network/store.ts
+++ src/network/store.ts
@@ -6,13 +6,14 @@ export {ObservableMap};
 
 export interface Request {
     id?: string;
-    status: "error" | "success" | "pending";
+    status: "error" | "ignored" | "pending" | "success";
     url: string;
 }
 
 @autobind
 export class RequestStore {
     readonly error = map<Request>({});
+    readonly ignored = map<Request>({});
     readonly pending = map<Request>({});
     readonly success = map<Request>({});
 
@@ -20,6 +21,7 @@ export class RequestStore {
     get count() {
         return {
             error: this.error.size,
+            ignored: this.ignored.size,
             pending: this.pending.size,
             success: this.success.size,
             total: this.pending.size + this.success.size + this.error.size
@@ -28,18 +30,20 @@ export class RequestStore {
 
     @action
     clearRequests() {
-        this.success.clear();
         this.error.clear();
+        this.ignored.clear();
         this.pending.clear();
+        this.success.clear();
     }
 
     @action
     updateRequest(request: Request) {
         request.id = request.id || v4();
         switch (request.status) {
-            case "success": this.success.set(request.id, request); break;
             case "error": this.error.set(request.id, request); break;
+            case "ignored": this.ignored.set(request.id, request); break;
             case "pending": this.pending.set(request.id, request); break;
+            case "success": this.success.set(request.id, request); break;
         }
 
         if (request.status !== "pending" && this.pending.has(request.id)) {
